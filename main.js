e3fded5cfe2aa6b05c62a46f715aa2f335f98272
Murodxon009 <ozodxonovm@gmail.com>
2024-10-07 17:37:40 +0500

    done

---
+++ main.js
@@ -0,0 +1,12 @@
+let num = prompt('Ismingizni kiriting');
+let number = prompt('Space ID kiriting');
+let num1 = prompt('Password kiriting');
+
+
+let obj = {
+    name: num,   
+    id: number, 
+    password: num1 
+};
+
+console.log(obj.name, obj.id, obj.password);
